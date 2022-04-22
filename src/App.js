a75b98f09cf279e93aef5797c9319b4ec33f1b1b
saneto044 <sa.jrneto@gmail.com>
2022-04-22 06:20:39 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -19,17 +19,54 @@ const stages = [
 ];
  
 function App() {
+  
   const [gameState,setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
-  console.log(words)
+  
+  const [pickedWord,setPickedWord] = useState("");
+  const [pickedCategory,setPickedCategory] = useState('')
+  const [letters , setLetters] = useState([]);
+  
+  const [guessedLetters , setGuessedLetters] = useState([]);
+  const [wrongLetters , setWrongLetters] = useState([])
+  const [guesses , setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+  
+  //console.log(words)
+ const pickWordAndCategory = useCallback(() => {
+   //picked a random category
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random()* Object.keys(categories).length)];
+    //pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)] 
+    //console.log(category,word)
+    return {category,word}
+  },[words])
   
   // starts the secret word game
-  const startGame = () => {
+  const startGame = useCallback(() => {
+ 
+    const { category, word } = pickWordAndCategory();
+
+    console.log(category, word);
+
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    // console.log(category, word);
+
+    setPickedCategory(category);
+    setPickedWord(word);
+    setLetters(wordLetters);
+
     setGameStage(stages[1].name);
-  };
+  }, [pickWordAndCategory]);
   //process the letter input
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    console.log(letter)
+    
   }
   const retry = () => {
     setGameStage(stages[0].name)
@@ -37,7 +74,17 @@ function App() {
   return (
     <div className="App">
       {gameState === "start" && <StartScreen startGame={startGame}/>}
-      {gameState === "game" && <Game verifyLetter={verifyLetter}/>}
+      {gameState === "game" && (
+      <Game 
+      verifyLetter={verifyLetter}
+      pickedWord={pickedWord}
+      pickedCategory={pickedCategory}
+      letters={letters}
+      guessedLetters={guessedLetters}
+      wrongLetters={wrongLetters}
+      guesses={guesses}
+      score={score}
+      /> )}
       {gameState === "end" && <GameOver retry={retry} />}
     </div>
   );
